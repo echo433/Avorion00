cce9ed67ad0387a37d573a9ad45502306f76ce10
echo433 <ahasnain2112@gmail.com>
2025-04-30 18:26:26 -0500

    Add files via upload

---
+++ modconfig (1).lua
@@ -0,0 +1,43 @@
+modLocation = ""
+forceEnabling = false
+
+mods =
+{
+  {workshopid = "3198978315"},
+  {workshopid = "3198967811"},
+  {workshopid = "1722412006"},
+  {workshopid = "1720259598"},
+  {workshopid = "2280867170"},
+  {workshopid = "2992808843"},
+  {workshopid = "2992808971"},
+  {workshopid = "2918443067"},
+  {workshopid = "3392854486"},
+  {workshopid = "3427505201"},
+  {workshopid = "2925318514"},
+  {workshopid = "2845715858"},
+  {workshopid = "2868925871"},
+  {workshopid = "2845684538"},
+  {workshopid = "2869275908"},
+  {workshopid = "2010035911"},
+  {workshopid = "2008725361"},
+  {workshopid = "3347925529"},
+  {workshopid = "3144281919"},
+  {workshopid = "3148341500"},
+  {workshopid = "3272120018"},
+  {workshopid = "3131377152"},
+  {workshopid = "3122496141"},
+  {workshopid = "2230451945"},
+  {workshopid = "2767486949"},
+  {workshopid = "2701395322"},
+  {workshopid = "2005799455"},
+  {workshopid = "2992808903"},
+}
+
+-- Allowed mods to run on the client such as UI mods.
+
+allowed =
+{
+  {id = ""},
+  {id = ""},
+  {id = ""},
+}
